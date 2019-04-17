5b966c441f4b2ee92215cee593f37babffdf5295
no7dw <no7david@gmail.com>
2019-04-18 01:24:41 +0800

    init

---
+++ mongo-transaction/transaction.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const mongoUri = 'mongodb://localhost:27017,localhost:37017,localhost:47017/test'
+const client = mongoose.createConnection(mongoUri, { replicaSet: 'rstest' })
+let Acc = client.model('Account', new mongoose.Schema({
+    name: String,
+    balance: Number
+}))
+async function initTest() {
+    await Acc.create({ 'name': 'James', 'balance': 3000 })
+    await Acc.create({ 'name': 'Wade', 'balance': 0 })
+}
+async function afterWork() {
+    await Acc.remove({ 'name': 'James' })
+    await Acc.remove({ 'name': 'Wade' })
+}
+async function transferTest(transfer) {
+    let session = await Acc.startSession()
+    session.startTransaction()
+    try {
+        const opts = { session, new: true }
+        let a = await Acc.findOneAndUpdate({
+            'name': 'James'
+        }, {
+            $inc: { 'balance': -transfer }
+        }, opts)
+        console.log(a.toObject());
+        let b = await Acc.findOneAndUpdate({
+            'name': 'Wade',
+        }, {
+            $inc: { 'balance': transfer }
+        }, opts)
+        console.log(b.toObject());
+        await session.commitTransaction()
+    } catch (err) {
+        session.abortTransaction()
+        console.error(err)
+    } finally {
+        session.endSession()
+    }
+
+}
+async function test() {
+    await afterWork()
+    await initTest()
+    await transferTest(100)
+        // await afterWork()
+}
+test()
